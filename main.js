c21e68a450e5c0474a911f2a2481863fdabdb8a9
David <92113063+gomesdevs@users.noreply.github.com>
2025-03-05 18:54:01 -0300

    Update main.js

---
+++ main.js
@@ -0,0 +1,16 @@
+// Revelar elementos no scroll
+window.addEventListener('scroll', reveal);
+
+function reveal() {
+    const reveals = document.querySelectorAll('.reveal');
+    
+    reveals.forEach(element => {
+        const windowHeight = window.innerHeight;
+        const elementTop = element.getBoundingClientRect().top;
+        const elementVisible = 150;
+
+        if (elementTop < windowHeight - elementVisible) {
+            element.classList.add('active');
+        }
+    });
+}
